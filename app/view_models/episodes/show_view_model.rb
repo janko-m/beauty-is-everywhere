f3bb5d559aec538c49c173c0cc668b060f7dbe66
Janko Marohnić <janko.marohnic@gmail.com>
2017-03-16 21:04:49 +1300

    Extract the remaining episodes/show.html.erb calls into view model

--- app/view_models/episodes/show_view_model.rb
+++ app/view_models/episodes/show_view_model.rb
@@ -3,6 +3,34 @@ class Episodes::ShowViewModel
     @episode = episode
   end
 
+  def edit_path
+    url_helpers.edit_episode_path(episode)
+  end
+
+  def draw_cabin?
+    episode.cabin?
+  end
+
+  def back_path
+    url_helpers.episodes_path
+  end
+
+  def episode_name
+    episode.name
+  end
+
+  def series_name
+    episode.series.name if episode.series.present?
+  end
+
+  def critter_count
+    episode.critters.to_i
+  end
+
+  def happy_little_tree_count
+    episode.happy_little_trees.to_i
+  end
+
   def paint_3peak_mountain
     if snowy_mountains.present?
       mountain_artist.paint(percentage: mountain_percentage).html_safe
@@ -23,5 +51,9 @@ class Episodes::ShowViewModel
 
   def mountain_artist
     @mountain_artist ||= MountainArtist.new
+  end
+
+  def url_helpers
+    Rails.application.routes.url_helpers
   end
 end
